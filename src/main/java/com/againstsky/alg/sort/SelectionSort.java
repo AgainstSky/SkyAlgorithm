87436af2312819d4c858e979dc029b3e06fa04e7
AgainstSky <againstskygod@outlook.com>
2022-09-05 18:02:44 +0800

    选择排序

--- src/main/java/com/againstsky/alg/sort/SelectionSort.java
+++ src/main/java/com/againstsky/alg/sort/SelectionSort.java
@@ -9,14 +9,12 @@ public class SelectionSort extends SkyAbstractSort {
 
     @Override
     public void sort(Comparable[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int min=i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (less(array[j],array[min])){
-                    min = j;
-                }
-            }
-            exch(array,i,min);
+        int N = array.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++)
+                if (less(array[j], array[min])) min = j;
+            exch(array, i, min);
         }
     }
 }
